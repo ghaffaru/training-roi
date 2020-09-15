0c676e26e8553d42b4a87b72b7d104765e73ab29
Ghaffaru <mudashiruagm@gmail.com>
2020-09-15 21:01:29 +0000

    add registeration view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="home">
     <Navbar />
+    <Register />
   </div>
 </template>
 
 <script>
 
 import Navbar from '../components/Navbar'
-
+import Register from './Register'
 export default {
   name: 'Home',
   components: {
-    Navbar
+    Navbar,
+    Register
   }
 }
 </script>
